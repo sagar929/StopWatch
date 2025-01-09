822a4b801927b68d02163ec4f2eb5632cae3d8c3
sagar929 <sagarsharma130804@gmail.com>
2025-01-09 13:29:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,113 @@
+
+// const clock =  document.querySelector(".clock")
+
+// let sec = 0;
+// let min = 0;
+// let hr = 0;
+
+// let interval;
+// let isclicked = false;
+
+// // it will start the count
+// const startwatch = () =>{  
+
+//    interval = setInterval(()=>{
+//     // if(sec <= 59){
+//     //     sec++;
+
+//     // }
+//     // else if (sec>59){
+//     //     min++;
+//     //     sec =0;
+//     // }
+//     // else if(min<=59){
+//     //     min++;
+
+//     // }
+//     // else if(min>59){
+//     //     hr++;
+//     //     min = 0;
+//     // }
+//     if(isclicked) return;
+//     isclicked = true;
+
+//     if (sec < 59) {
+//         sec++;
+//     } 
+    
+//     else {
+//         sec = 0;
+
+//         min++;
+
+//         if(min>59){
+//             hr++;
+//             min = 0;
+//         }
+//         // if (min < 59) {
+//         //     min++;
+//         // } 
+//         // else {
+//         //     min = 0;
+//         //     hr++;
+//         //      }   
+//         }   
+    
+
+//     const seconds = String(sec).padStart(2,"0");
+//     const minutes = String(min).padStart(2,"0");
+//     const hours = String(hr).padStart(2,"0");
+
+//     console.log(`${hours} :${minutes} :${seconds}`)
+        
+//      clock.innerHTML = `${hours} :${minutes} :${seconds}`; 
+//     },1000);
+// };
+
+// // clear interval will stop the interval
+// const stopwatch = () => {
+    
+//     clearInterval(interval);
+//     isclicked = false;
+// };
+
+const clock =  document.querySelector(".clock");
+
+let sec = 0;
+let min = 0;
+let hr = 0;
+
+let interval;
+let isclicked = false;
+
+// Start the stopwatch function
+const startwatch = () => {  
+    if (isclicked) return; 
+    isclicked = true;
+
+    interval = setInterval(() => {
+        if (sec < 59) {
+            sec++;
+        } else {
+            sec = 0;
+            min++;
+            if (min > 59) {
+                hr++;
+                min = 0;
+            }
+        }
+
+        const seconds = String(sec).padStart(2, "0");
+        const minutes = String(min).padStart(2, "0");
+        const hours = String(hr).padStart(2, "0");
+
+        console.log(`${hours}:${minutes}:${seconds}`);
+        clock.innerHTML = `${hours}:${minutes}:${seconds}`; 
+    }, 1000);
+};
+
+// Stop the stopwatch function
+const stopwatch = () => {
+    clearInterval(interval);
+    isclicked = false; 
+};
